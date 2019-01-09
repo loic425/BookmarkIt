36beb58def471be62dcfa0f3975dc7c0c134d8a3
Loic Fremont <lc.fremont@gmail.com>
2019-01-09 09:58:43 +0100

    OembedClient specs

--- spec/App/Client/OembedClientSpec.php
+++ spec/App/Client/OembedClientSpec.php
@@ -3,13 +3,25 @@
 namespace spec\App\Client;
 
 use App\Client\OembedClient;
+use GuzzleHttp\Client;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Psr\Http\Message\ResponseInterface;
 
 class OembedClientSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function let(Client $client)
     {
-        $this->shouldHaveType(OembedClient::class);
+        $this->beConstructedWith($client, '/path?url=%s');
+    }
+
+    function it_calls_client_request(Client $client, ResponseInterface $response): void
+    {
+        $url = 'http:example.com';
+        $client->get('/path?url=http:example.com')->willReturn($response);
+
+        $client->get('/path?url=http:example.com')->shouldBeCalled();
+
+        $this->request($url);
     }
 }
